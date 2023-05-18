c60d839533f7a9334d6dba28f6914e77d3f51fcc
jknfic <jk2922@student.uni-lj.si>
2023-05-18 23:55:45 +0200

    Re-Setup of angular application, corrected routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {MapLeafletComponent} from "./components/map-leaflet/map-leaflet.component";
+import {TestComponent} from "./components/test/test.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: 'map', component: MapLeafletComponent},
+    {path: 'test', component: TestComponent},
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapLeafletComponent } from './components/map-leaflet/map-leaflet.component';
+import { TestComponent } from './components/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapLeafletComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
