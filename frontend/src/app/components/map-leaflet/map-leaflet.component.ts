96fbfee1d2da25cbe4daf695aa095537aa9baa34
jknfic <jk2922@student.uni-lj.si>
2023-05-19 00:08:58 +0200

    Added a leaflet map to map-leaflet component that shows Ljubljana

--- frontend/src/app/components/map-leaflet/map-leaflet.component.ts
+++ frontend/src/app/components/map-leaflet/map-leaflet.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map-leaflet',
   templateUrl: './map-leaflet.component.html',
   styleUrls: ['./map-leaflet.component.css']
 })
-export class MapLeafletComponent {
+export class MapLeafletComponent implements AfterViewInit{
+
+  private map: any;
+
+  constructor() {
+  }
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 46.05108, 14.50513 ],
+      zoom: 13
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
 
 }
