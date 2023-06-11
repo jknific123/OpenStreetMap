e42abccdc554f9c54076e8685788f185656325d8
jknfic <jk2922@student.uni-lj.si>
2023-06-11 17:48:14 +0200

    Added getAllUsers api

--- backend/app/controllers/user.controller.js
+++ backend/app/controllers/user.controller.js
@@ -1,15 +1,43 @@
-exports.allAccess = (req, res) => {
+const mongoose = require('mongoose');
+const User = mongoose.model('UserModel');
+
+
+const allAccess = (req, res) => {
   res.status(200).send("Public Content.");
 };
 
-exports.userBoard = (req, res) => {
+const userBoard = (req, res) => {
   res.status(200).send("User Content.");
 };
 
-exports.adminBoard = (req, res) => {
+const adminBoard = (req, res) => {
   res.status(200).send("Admin Content.");
 };
 
-exports.moderatorBoard = (req, res) => {
+const moderatorBoard = (req, res) => {
   res.status(200).send("Moderator Content.");
+};
+
+const getAllUsers = (req, res) => {
+    User
+        .find()
+        .exec((error, users) => {
+            if (!users) {
+                return res.status(404).json({
+                    "msg":
+                        "Can not find users."
+                });
+            } else if (error) {
+                return res.status(500).json(error);
+            }
+            res.status(200).json(users);
+        });
+};
+
+module.exports = {
+  getAllUsers,
+  allAccess,
+  userBoard,
+  adminBoard,
+  moderatorBoard,
 };
--- backend/app/routes/user.routes.js
+++ backend/app/routes/user.routes.js
@@ -10,6 +10,8 @@ module.exports = function(app) {
     next();
   });
 
+  app.get("/api/users/all", controller.getAllUsers);
+
   app.get("/api/test/all", controller.allAccess);
 
   app.get("/api/test/user", [authJwt.verifyToken], controller.userBoard);
