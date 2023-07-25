4400dc1c68e47fcabcb3d97cec0039eecc8dec13
jknfic <jk2922@student.uni-lj.si>
2023-07-25 19:38:48 +0200

    Added red color to first marker on a map

--- frontend/src/app/components/map-leaflet/map-leaflet.component.ts
+++ frontend/src/app/components/map-leaflet/map-leaflet.component.ts
@@ -47,6 +47,17 @@ export class MapLeafletComponent implements AfterViewInit {
     tiles.addTo(this.map);
   }
 
+  redIcon = new L.Icon({
+      iconUrl:
+        "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
+      shadowUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
+    });
+
   ngAfterViewInit(): void {
     this.initMap();
     this.markerService.makeCapitalMarkers(this.map);
@@ -65,7 +76,9 @@ export class MapLeafletComponent implements AfterViewInit {
       this.map.removeLayer(this.marker);
     }
 
-    this.marker = L.marker([e.latlng.lat, e.latlng.lng]); // add the marker onclick
+    this.marker = L.marker([e.latlng.lat, e.latlng.lng], {
+      icon: this.redIcon
+    }); // add the marker onclick
     this.marker.addTo(this.map);
 
     // klic na be osmnx
@@ -82,6 +95,7 @@ export class MapLeafletComponent implements AfterViewInit {
         console.log('Error getting POIS: ', err);
       }
     });
+
   }
 
   public reloadMap() {
